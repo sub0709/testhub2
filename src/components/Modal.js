52b6d079e5364e76297bef56b9e68be84ee86978
swpark <cse8025@gmail.com>
2020-06-16 17:44:19 +0900

    모달 다이얼로그 처리

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,19 +3,21 @@ import Bs4Modal from 'react-bootstrap4-modal';
 
 class Modal extends Component {
   render() {
+    console.log(this.props)
     return (
 			<Bs4Modal visible={this.props.visible}>
 				<div className="modal-header">
-					<h5 className="modal-title">준비</h5>
-					<button type="button" className="close">
-						<span aria-hidden="true">&times;</span>
+					<h5 className="modal-title">{this.props.title}</h5>
+					<button type="button" className="close" onClick={() => this.props.onClose()}>
+						<span aria-hidden="true" >&times;</span>
 					</button>
 				</div>
 				<div className="modal-body">
+          {this.props.body}
 				</div>
 				<div className="modal-footer">
 					<button type="button" className="btn btn-primary">
-						닫기
+						{this.props.children}
 					</button>
 				</div>
 			</Bs4Modal>
--- src/pages/sample/Welcome.js
+++ src/pages/sample/Welcome.js
@@ -562,3 +562,24 @@ class Reservation extends React.Component {
     );
   }
 }
+
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+    </FancyBorder>
+  );
+}
